9241f65dddc2c24794663fc36925749fb9342f43
toral-7 <toralb.ut@gmail.com>
2026-06-03 20:01:58 +0530

    v1.3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import PlantNutrientDetector from './PlantDoc_Professional';
+import PlantDoc from './PlantDoc_Professional';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <PlantNutrientDetector />
+      <PlantDoc />
     </div>
   );
 }
